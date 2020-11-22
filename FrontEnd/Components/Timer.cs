448a8d63259b4146e0fe964af9664172647a8e0d
Bryan <bryanegbert561@gmail.com>
2020-11-22 13:24:42 +0700

    Add stop button and delete bootstrap

--- FrontEnd/Components/Timer.cs
+++ FrontEnd/Components/Timer.cs
@@ -9,33 +9,39 @@ namespace FrontEnd.Components
         public int Hours { get; set; } = 13;
         public int Minutes { get; set; } = 1;
         public int Seconds { get; set; } = 1;
-        private System.Timers.Timer aTimer;
+        public bool StopButtonState { get; set; } = true;
+        public bool StartButtonState { get; set; } = false;
+        private static System.Timers.Timer aTimer;
+        public void StopTimer()
+        {
+            aTimer.Stop();
+            aTimer.Dispose();
+            StopButtonState = true;
+            StartButtonState = false;
+        }
         public void StartTimer() 
         {
             aTimer = new System.Timers.Timer(1000);
             aTimer.Elapsed += CountDownTimer;
-            if(aTimer.Enabled == true)
-            {
-                aTimer.Enabled = false;
-            } else
-            {
-                aTimer.Enabled = true;
-            }
+            aTimer.Start(); 
+            StopButtonState = false;   
+            StartButtonState = true;
         }
         public void CountDownTimer(Object source, ElapsedEventArgs e)
         {
-            if(Seconds == 0)
+            if(Seconds == 0 && Minutes > 0)
             {
                 Minutes -= 1;
                 Seconds = 59;
-            } else if(Minutes == 0)
+            } else if(Minutes == 0 && Seconds == 0 && Hours > 0)
             {
                 Hours -= 1;
                 Minutes = 59;
                 Seconds = 59;
             } else if(Hours == 0 && Minutes == 0 && Seconds == 0)
             {
-                aTimer.Enabled = false;
+                aTimer.Stop();
+                aTimer.Dispose();
             } else
             {
                 Seconds -= 1;
