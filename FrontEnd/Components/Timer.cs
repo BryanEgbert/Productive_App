c30b36608e8a7591554bfe4187bce5ecfabe7990
Bryan <bryanegbert561@gmail.com>
2020-11-23 14:31:41 +0700

    Add set timer button

--- FrontEnd/Components/Timer.cs
+++ FrontEnd/Components/Timer.cs
@@ -1,31 +1,57 @@
 using System;
 using System.Timers;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Components;
 
 namespace FrontEnd.Components
 {
+    public class TimerStructure
+    {
+        public  int Id { get; set; }
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }      
+    }
     public class Timer : ComponentBase
     {
-        public int Hours { get; set; } = 13;
-        public int Minutes { get; set; } = 1;
-        public int Seconds { get; set; } = 1;
-        public bool StopButtonState { get; set; } = true;
-        public bool StartButtonState { get; set; } = false;
+        public List<TimerStructure> timerCollections = new List<TimerStructure>()
+        {
+            new TimerStructure(){ Id = 1, Hours = 0, Minutes = 30, Seconds = 0 },
+            new TimerStructure(){ Id = 2, Hours = 1, Minutes = 0, Seconds = 0 }
+        };
+
+        public int Index { get; private set; }
+        public int Hours { get; set; } = 0;
+        public int Minutes { get; set; } = 0;
+        public int Seconds { get; set; } = 0;
+        public bool StopButtonIsDisabled { get; set; } = true;
+        public bool StartButtonIsDisabled { get; set; } = false;
         private static System.Timers.Timer aTimer;
+        public void SetTimer(int value)
+        {
+            this.Index = value - 1;
+            Hours = timerCollections[Index].Hours;
+            Minutes = timerCollections[Index].Minutes;
+            Seconds = timerCollections[Index].Seconds;
+        }
         public void StopTimer()
         {
             aTimer.Stop();
             aTimer.Dispose();
-            StopButtonState = true;
-            StartButtonState = false;
+
+            StopButtonIsDisabled = true;
+            StartButtonIsDisabled = false;
+
+            Console.WriteLine($"{Hours}:{Minutes}:{Seconds}");
         }
         public void StartTimer() 
         {
             aTimer = new System.Timers.Timer(1000);
             aTimer.Elapsed += CountDownTimer;
             aTimer.Start(); 
-            StopButtonState = false;   
-            StartButtonState = true;
+
+            StopButtonIsDisabled = false;   
+            StartButtonIsDisabled = true;
         }
         public void CountDownTimer(Object source, ElapsedEventArgs e)
         {
@@ -42,6 +68,9 @@ namespace FrontEnd.Components
             {
                 aTimer.Stop();
                 aTimer.Dispose();
+
+                StopButtonIsDisabled = true;
+                StartButtonIsDisabled = false;
             } else
             {
                 Seconds -= 1;
