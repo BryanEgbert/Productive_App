8f89b5a9e30a20be8b1f88aa0e9374426680972f
Bryan <bryanegbert561@gmail.com>
2020-11-30 23:04:47 +0700

    Modify Startup

--- BackEnd/Startup.cs
+++ BackEnd/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using BackEnd.Models;
 
 namespace BackEnd
 {
@@ -26,7 +28,7 @@ namespace BackEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<UserContext>(opt => opt.UseInMemoryDatabase("User"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
