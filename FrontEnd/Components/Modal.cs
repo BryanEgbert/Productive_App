5f4d5a34d704bf77a30b936b1e6df8214320800f
Bryan <bryanegbert561@gmail.com>
2020-12-06 17:54:01 +0700

    Add modal page functionality(Incomplete)

---
+++ FrontEnd/Components/Modal.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.AspNetCore.Components;
+
+namespace FrontEnd.Components
+{
+    public class Modal : ComponentBase
+    {
+        [Parameter]
+        public bool IsOpened { get; set; }
+        public string cssModal => IsOpened ? "show" : "hide"; 
+        [Parameter]
+        public string Title { get; set; }
+
+        public void hideModal()
+        {
+            IsOpened = false;
+            Console.WriteLine($"{cssModal}");
+        }
+    }
+}
