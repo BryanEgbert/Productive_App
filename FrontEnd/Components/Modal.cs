5c9f6dbe787949adc27184ddba60d905ff26e2d9
Bryan <bryanegbert561@gmail.com>
2020-12-12 10:56:39 +0700

    Fix bug on modal page

--- FrontEnd/Components/Modal.cs
+++ FrontEnd/Components/Modal.cs
@@ -1,14 +1,16 @@
-using System;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace FrontEnd.Components
 {
     public class Modal : ComponentBase
     {
         [Parameter]
-        public bool IsOpened { get; set; }
-        public string cssModal => IsOpened ? "show" : "hide"; 
-        [Parameter]
         public string Title { get; set; }
+        [Parameter]
+        public bool IsOpened { get; set; }
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnExit { get; set; }
+        public string cssModal => IsOpened ? "show" : "hide"; 
     }
 }
