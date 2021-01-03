8a5a37200fbdc2389073648e0f2332bd19a93d52
Bryan <bryanegbert561@gmail.com>
2021-01-03 19:15:05 +0700

    Connect to gRPC server

--- FrontEnd/Program.cs
+++ FrontEnd/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+using BackEnd;
 
 namespace FrontEnd
 {
@@ -18,8 +21,19 @@ namespace FrontEnd
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(services => 
+            {
+                var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler());
+                var channel = GrpcChannel.ForAddress("https://localhost:5000", new GrpcChannelOptions
+                    { 
+                        HttpHandler = httpHandler
+                    });
+
+                return new Greeter.GreeterClient(channel);
+            });
 
             await builder.Build().RunAsync();
+
         }
     }
 }
