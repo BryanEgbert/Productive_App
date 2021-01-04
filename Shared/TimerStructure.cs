36457b40729188385150ec72ce6c1b328e9f3c05
Bryan <bryanegbert561@gmail.com>
2021-01-04 09:56:33 +0700

    Add shared folder

---
+++ Shared/TimerStructure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shared
+{
+    public class TimerStructure
+    {
+        public  int Id { get; set; }
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }      
+    }
+}
