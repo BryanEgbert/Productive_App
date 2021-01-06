93dcf151210124f5ad8d31f824ca9c92233ad13b
Bryan <bryanegbert561@gmail.com>
2021-01-06 14:43:08 +0700

    Add messages to user.proto

--- BackEnd/Services/GreeterService.cs
+++ BackEnd/Services/GreeterService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
-using Global;
+using Global.Protos;
 
 namespace BackEnd
 {
--- BackEnd/Services/UserService.cs
+++ BackEnd/Services/UserService.cs
@@ -4,17 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
-using Global;
+using Global.Protos;
+using static Global.Protos.UserInfo.Types;
 
 namespace BackEnd
 {
     public class UserService : User.UserBase
     
     {
-        private readonly ILogger<UserService> logger_logger;
+        private readonly ILogger<UserService> _logger;
         public UserService(ILogger<UserService> logger)
         {
             _logger = logger;
         }
-    }
+
+        public override Task<UserInfo> GetUser(UserIdentity request, ServerCallContext context)
+        {
+            return Task.FromResult(new UserInfo
+            {
+                Name = request.Name,
+                Timer={
+                    new TimerStructure{ Id = 4, Hours = 0, Minutes = 30 , Seconds = 0 }
+                }
+            });
+        }
+    } 
 }
