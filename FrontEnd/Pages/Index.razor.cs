46baff3929a23467d99d5d4e844725c36daa166d
Bryan <bryanegbert561@gmail.com>
2021-01-26 23:08:05 +0700

    Add to-do list core function (Incomplete)

--- FrontEnd/Pages/Index.razor.cs
+++ FrontEnd/Pages/Index.razor.cs
@@ -1,21 +1,24 @@
 using System;
 using FrontEnd.Components;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace FrontEnd.Pages
 {
     public partial class Index
     {
         public bool IsCompleted { get; set; }
-        public List<string> ToDoListCollection { get; set; } = new List<string>()
+        public string Description { get; set; }
+        public List<string> ToDoListCollection { get; set; }
+        public void AddList(KeyboardEventArgs e)
         {
-            "Walk",
-            "Eat",
-            "Sleep"
-        };
-        public void AddList()
-        {
-
+            if (e.Key == "Enter" && !string.IsNullOrWhiteSpace(Description) || 
+                e.Key == "NumpadEnter" && !string.IsNullOrWhiteSpace(Description))
+            {
+                ToDoListCollection.Add($"{Description}");
+                Description = null;
+            } else
+            {}
         }
     }
 }
