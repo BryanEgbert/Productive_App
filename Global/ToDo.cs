eb56fedaea6710a08515424c42b8ec4afaac16e7
Bryan <bryanegbert561@gmail.com>
2021-01-30 15:35:04 +0700

    Fix to-do list error

---
+++ Global/ToDo.cs
@@ -0,0 +1,9 @@
+namespace Global
+{
+    public class ToDo
+    {
+        public int ID { get; set; }
+        public bool IsCompleted { get; set; }
+        public string Description { get; set; }
+    }
+}
